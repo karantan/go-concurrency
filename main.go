86663c7a01ddaad88d5e5a89fe0df76a654371fc
Gasper Vozel <gv@niteo.co>
2021-09-10 08:41:15 +0200

    Add ssh runner and update main.go

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	ssh "go-concurrency/async-workers"
+	"go-concurrency/logger"
+	"time"
+)
+
+var log = logger.New("main")
+
+var COMMAND = "uname -a"
 
 func main() {
-	fmt.Println("See packages for examples")
+	defer timeTrack(time.Now(), "main")
+
+	serverHostnames := []string{"foo", "bar"}
+	var jobs []ssh.Job
+
+	for i, hostname := range serverHostnames {
+		// Run SSH command:
+		jobs = append(jobs, ssh.Job{
+			JobNumber: i,
+			Hostname:  hostname,
+			Command:   fmt.Sprintf("%s %s", hostname, COMMAND),
+			App:       "ssh",
+		})
+	}
+
+	results := ssh.Run(ssh.GenericJobRunner, jobs)
+	for _, r := range results {
+		log.Infof("[%d/%d %s] %s", r.JobNumber+1, len(results), r.Hostname, r.Result)
+	}
+
+}
+
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Infof("%s took %s", name, elapsed)
 }
